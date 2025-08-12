57ed5bf321a6c11f1618e10404a5c447e0aee8ae
joykbiswas <joykumarbiswas100@gmail.com>
2025-08-12 23:52:22 +0600

    first commit

---
+++ src/pages/homePage/homePage.tsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-red-500 font-bold">Home..</h1>
+        </div>
+    );
+};
+
+export default HomePage;
